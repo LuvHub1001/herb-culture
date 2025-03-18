e046dc48727463db2e41706b84080f5908057795
dy7791 <dy7791@naver.com>
2025-03-18 15:14:47 +0900

    add:: Footer

---
+++ src/components/common/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <div className="flex min-w-screen h-30 justify-center items-center bg-black text-white ">
+      Copyright © herb
+    </div>
+  );
+}
+
+export default Footer;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 // Common
 import ErrorBoundary from "./common/ErrorBoundary";
+import Footer from "./common/Footer";
 import Header from "./common/Header";
 import Loading from "./common/Loading";
 
-export { ErrorBoundary, Header, Loading };
+export { ErrorBoundary, Footer, Header, Loading };
