f7b3dba0513af34607e1dba8cf70b138b3e7f64c
dy7791 <dy7791@naver.com>
2025-03-18 18:35:51 +0900

    add:: RenderComponent

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,7 @@ import ErrorBoundary from "./common/ErrorBoundary";
 import Footer from "./common/Footer";
 import Header from "./common/Header";
 import Loading from "./common/Loading";
+import RenderComponent from "./common/RenderComponent";
 
 // etc
 import SearchBar from "./etc/SearchBar";
@@ -10,4 +11,12 @@ import SearchBar from "./etc/SearchBar";
 // event
 import EventFetch from "./event/EventFetch";
 
-export { ErrorBoundary, Footer, Header, Loading, SearchBar, EventFetch };
+export {
+  ErrorBoundary,
+  Footer,
+  Header,
+  Loading,
+  RenderComponent,
+  SearchBar,
+  EventFetch,
+};
