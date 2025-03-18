916cf8f606646c20ecb2eaaef489e693d2b697cb
dy7791 <dy7791@naver.com>
2025-03-18 17:29:56 +0900

    add:: useFetch

---
+++ src/hooks/fetch/useFetch.ts
@@ -0,0 +1,44 @@
+import { useState, useCallback, useEffect } from "react";
+import { AxiosRequestConfig } from "axios";
+import { useAsyncError } from "../";
+
+type Nullable<T> = T | null | undefined;
+
+const useFetch = <Params extends string, FetchResult>(
+  fetch: (params: Params, config?: AxiosRequestConfig) => Promise<FetchResult>,
+  params: Params,
+  config?: AxiosRequestConfig,
+): Nullable<FetchResult> => {
+  const { catchAsyncError } = useAsyncError();
+
+  const [_promise, _setPromise] = useState<Promise<void>>();
+  const [_status, _setStatus] = useState<"pending" | "fulfilled" | "error">(
+    "pending",
+  );
+  const [_result, _setResult] = useState<Nullable<FetchResult>>(null);
+
+  const resolve = useCallback(
+    (res: FetchResult) => {
+      _setStatus("fulfilled");
+      _setResult(res);
+    },
+    [params],
+  );
+
+  useEffect(() => {
+    _setStatus("pending");
+    _setPromise(
+      fetch(params, config)
+        .then(resolve)
+        .catch((error: Error) => catchAsyncError(error)),
+    );
+  }, [params]);
+
+  if (_promise && _status === "pending") {
+    throw _promise;
+  }
+
+  return _result;
+};
+
+export default useFetch;
