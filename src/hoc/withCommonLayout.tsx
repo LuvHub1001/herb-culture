f75e7456683cd543cb25b5c23146cbd92a99fb4b
dy7791 <dy7791@naver.com>
2025-03-18 16:11:22 +0900

    remove:: unnessary line

--- src/hoc/withCommonLayout.tsx
+++ src/hoc/withCommonLayout.tsx
@@ -4,15 +4,13 @@ import { Header, Footer } from "../components";
 function withCommonLayout(Component: ComponentType) {
   const WrappedComponent = () => {
     return (
-      <div>
+      <>
         <Header />
         <div className="min-h-[calc(h-screen - h-105 - h-30)]">
           <Component />
         </div>
-        <div>
-          <Footer />
-        </div>
-      </div>
+        <Footer />
+      </>
     );
   };
 
