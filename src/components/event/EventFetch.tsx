682c81a5dc24bc270dba9cd9c1848d26d21ccdc3
dy7791 <dy7791@naver.com>
2025-03-19 16:19:46 +0900

    fix:: Event 내용 배치

--- src/components/event/EventFetch.tsx
+++ src/components/event/EventFetch.tsx
@@ -3,20 +3,32 @@ import { useFetch } from "../../hooks";
 import { EventType } from "../../types/EventType";
 
 function EventFetch() {
-  const eventRes = useFetch(get, "/culturalEventInfo/1/50");
+  const eventRes = useFetch(get, "/culturalEventInfo/1/9");
 
   return (
     eventRes && (
       <div className="flex w-screen justify-center mt-10    ">
-        <div className="grid grid-cols-3 gap-6">
+        <div className="grid w-1/2 grid-cols-3 gap-6">
           {eventRes.data.culturalEventInfo.row.map(
             (item: EventType, idx: number) => {
               return (
-                <div key={idx}>
-                  <img className="w-70 h-70 rounded-2xl" src={item.MAIN_IMG} />
-                  <div>지역: {item.GUNAME}</div>
-                  <div>기간: {item.DATE}</div>
-                  <div>비용: {item.USE_FEE === "" ? "무료" : item.USE_FEE}</div>
+                <div key={idx} className="border-2 rounded-2xl">
+                  <a
+                    className="cursor-pointer"
+                    href={item.ORG_LINK}
+                    target="_blank"
+                  >
+                    <img
+                      className="w-full h-70 rounded-tl-xl rounded-tr-xl"
+                      src={item.MAIN_IMG}
+                    />
+                  </a>
+                  <div className="mt-2">
+                    <div>{item.TITLE}</div>
+                    <div>구분: {item.CODENAME}</div>
+                    <div>지역: {item.GUNAME}</div>
+                    <div>기간: {item.DATE}</div>
+                  </div>
                 </div>
               );
             },
