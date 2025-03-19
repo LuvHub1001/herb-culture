7b20bd81f3ad9cc191b9de4c0e0922610686877b
dy7791 <dy7791@naver.com>
2025-03-19 14:38:28 +0900

    add:: EventType

---
+++ src/types/EventType.ts
@@ -0,0 +1,12 @@
+export type EventType = {
+  CODENAME: string;
+  DATE: string;
+  END_DATE: string;
+  ETC_DESC?: string;
+  GUNAME: string;
+  HMPG_ADDR: string;
+  PLACE: string;
+  TITLE: string;
+  MAIN_IMG: string;
+  USE_FEE: string;
+};
