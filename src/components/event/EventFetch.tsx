462bd1d5d8cd2d0f2553c172b38f9dc23fb325ed
dy7791 <dy7791@naver.com>
2025-03-19 17:46:11 +0900

    fix:: style, 제목 길이 조정

--- src/components/event/EventFetch.tsx
+++ src/components/event/EventFetch.tsx
@@ -7,12 +7,15 @@ function EventFetch() {
 
   return (
     eventRes && (
-      <div className="flex w-screen justify-center mt-10    ">
+      <div className="flex w-screen justify-center mt-10">
         <div className="grid w-1/2 grid-cols-3 gap-6">
           {eventRes.data.culturalEventInfo.row.map(
             (item: EventType, idx: number) => {
               return (
-                <div key={idx} className="border-2 rounded-2xl">
+                <div
+                  key={idx}
+                  className="border-2 border-[#EFEFEF] rounded-2xl"
+                >
                   <a
                     className="cursor-pointer"
                     href={item.ORG_LINK}
@@ -23,8 +26,12 @@ function EventFetch() {
                       src={item.MAIN_IMG}
                     />
                   </a>
-                  <div className="mt-2">
-                    <div>{item.TITLE}</div>
+                  <div className="p-2">
+                    <div>
+                      {item.TITLE.length > 10
+                        ? item.TITLE.slice(0, 24) + "..."
+                        : item.TITLE}
+                    </div>
                     <div>구분: {item.CODENAME}</div>
                     <div>지역: {item.GUNAME}</div>
                     <div>기간: {item.DATE}</div>
