af9d11eb5453a45552c1b66b217a671f87c4c98f
dy7791 <dy7791@naver.com>
2025-03-20 15:49:42 +0900

    fix:: EventFetch에 Pagination 적용

--- src/components/event/EventFetch.tsx
+++ src/components/event/EventFetch.tsx
@@ -1,47 +1,77 @@
-import { get } from "../../apis";
+import { useEffect, useState } from "react";
+import { usePagination } from "../../hooks";
 import { useFetch } from "../../hooks";
+import { Pagination } from "../";
 import { EventType } from "../../types/EventType";
+import { get } from "../../apis";
 
 function EventFetch() {
-  const eventRes = useFetch(get, "/culturalEventInfo/1/9");
+  const [totalItems, setTotalItems] = useState<number>(0);
+
+  const { currentPage, setCurrentPage } = usePagination({
+    totalItems,
+    divider: 10,
+  });
+
+  const startIndex = (currentPage - 1) * 10 + 1;
+  const endIndex = currentPage * 10;
+
+  const eventRes = useFetch(
+    get,
+    `/culturalEventInfo/${startIndex}/${endIndex}`,
+  );
+
+  useEffect(() => {
+    if (eventRes?.data?.culturalEventInfo?.list_total_count) {
+      const totalCount = eventRes.data.culturalEventInfo.list_total_count;
+      setTotalItems(totalCount);
+    }
+  }, [eventRes]);
 
   return (
     eventRes && (
-      <div className="flex w-screen justify-center mt-10">
-        <div className="grid w-10/12 grid-cols-5 gap-6">
-          {eventRes.data.culturalEventInfo.row.map(
-            (item: EventType, idx: number) => {
-              return (
-                <div
-                  key={idx}
-                  className="border-2 border-[#EFEFEF] rounded-2xl"
-                >
-                  <a
-                    className="cursor-pointer"
-                    href={item.ORG_LINK}
-                    target="_blank"
+      <>
+        <div className="flex w-screen justify-center mt-10">
+          <div className="grid w-10/12 grid-cols-5 gap-6">
+            {eventRes.data.culturalEventInfo?.row?.map(
+              (item: EventType, idx: number) => {
+                return (
+                  <div
+                    key={idx}
+                    className="border-2 border-[#EFEFEF] rounded-2xl"
                   >
-                    <img
-                      className="w-full h-70 rounded-tl-xl rounded-tr-xl"
-                      src={item.MAIN_IMG}
-                    />
-                  </a>
-                  <div className="p-2">
-                    <div>
-                      {item.TITLE.length > 10
-                        ? item.TITLE.slice(0, 24) + "..."
-                        : item.TITLE}
+                    <a
+                      className="cursor-pointer"
+                      href={item.ORG_LINK}
+                      target="_blank"
+                    >
+                      <img
+                        className="w-full h-70 rounded-tl-xl rounded-tr-xl"
+                        src={item.MAIN_IMG}
+                      />
+                    </a>
+                    <div className="p-2">
+                      <div>
+                        {item.TITLE.length > 10
+                          ? item.TITLE.slice(0, 24) + "..."
+                          : item.TITLE}
+                      </div>
+                      <div>구분: {item.CODENAME}</div>
+                      <div>지역: {item.GUNAME}</div>
+                      <div>기간: {item.DATE}</div>
                     </div>
-                    <div>구분: {item.CODENAME}</div>
-                    <div>지역: {item.GUNAME}</div>
-                    <div>기간: {item.DATE}</div>
                   </div>
-                </div>
-              );
-            },
-          )}
+                );
+              },
+            )}
+          </div>
         </div>
-      </div>
+        <Pagination
+          totalItems={totalItems}
+          divider={10}
+          onPageChange={setCurrentPage}
+        />
+      </>
     )
   );
 }
