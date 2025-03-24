325f591bcc227f26abf6cec260d5738d1dff58c1
dy7791 <dy7791@naver.com>
2025-03-24 13:43:06 +0900

    fix:: EventSkeleton 적용용

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,12 +1,27 @@
 import { Suspense } from "react";
-import { SearchBar, EventFetch, AddressButton, Loading } from "../components";
+import {
+  SearchBar,
+  EventFetch,
+  AddressButton,
+  EventSkeleton,
+} from "../components";
 
 function MainPage() {
   return (
     <>
       <SearchBar />
       <AddressButton />
-      <Suspense fallback={<Loading />}>
+      <Suspense
+        fallback={
+          <div className="flex w-screen justify-center mt-10">
+            <div className="grid w-10/12 grid-cols-5 gap-6">
+              {[...Array(10)].map((_, idx) => (
+                <EventSkeleton key={idx} />
+              ))}
+            </div>
+          </div>
+        }
+      >
         <EventFetch />
       </Suspense>
     </>
