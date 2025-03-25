e3b5ae30782540d5acb9aca65af7adacb7282be7
dy7791 <dy7791@naver.com>
2025-03-25 15:46:55 +0900

    fix:: Suspense 위치 변경

--- src/routers/PublicRouter.tsx
+++ src/routers/PublicRouter.tsx
@@ -1,17 +1,20 @@
-import { lazy } from "react";
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { withCommonLayout } from "../hoc";
+import { Loading } from "../components";
 
 const MainPage = lazy(() => import("../pages/MainPage"));
 const WrappedMainPage = withCommonLayout(MainPage);
 
 function PublicRouter() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<WrappedMainPage />} />
-      </Routes>
-    </BrowserRouter>
+    <Suspense fallback={<Loading />}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<WrappedMainPage />} />
+        </Routes>
+      </BrowserRouter>
+    </Suspense>
   );
 }
 
