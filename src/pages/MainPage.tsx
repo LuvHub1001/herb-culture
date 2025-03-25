51879130193c25ef793ca1fba7626ff5d1d58214
dy7791 <dy7791@naver.com>
2025-03-25 15:45:50 +0900

    fix:: 메인페이지에서 Suspense 제거거

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,32 +1,11 @@
-import { Suspense } from "react";
-import {
-  SearchBar,
-  EventFetch,
-  AddressButton,
-  EventSkeleton,
-  Loading,
-} from "../components";
+import { SearchBar, EventFetch, AddressButton } from "../components";
 
 function MainPage() {
   return (
     <>
       <SearchBar />
-      <Suspense fallback={<Loading />}>
-        <AddressButton />
-      </Suspense>
-      <Suspense
-        fallback={
-          <div className="flex w-screen justify-center mt-10">
-            <div className="grid w-10/12 grid-cols-5 gap-6">
-              {[...Array(10)].map((_, idx) => (
-                <EventSkeleton key={idx} />
-              ))}
-            </div>
-          </div>
-        }
-      >
-        <EventFetch />
-      </Suspense>
+      <AddressButton />
+      <EventFetch />
     </>
   );
 }
