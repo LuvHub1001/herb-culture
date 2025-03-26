0d7bb5a50168788677546b617f2c13f1fa64fe79
dy7791 <dy7791@naver.com>
2025-03-26 16:06:49 +0900

    add:: SearchPage

--- src/routers/PublicRouter.tsx
+++ src/routers/PublicRouter.tsx
@@ -6,12 +6,16 @@ import { Loading } from "../components";
 const MainPage = lazy(() => import("../pages/MainPage"));
 const WrappedMainPage = withCommonLayout(MainPage);
 
+const SearchPage = lazy(() => import("../pages/SearchPage"));
+const WrappedSearchPage = withCommonLayout(SearchPage);
+
 function PublicRouter() {
   return (
     <Suspense fallback={<Loading />}>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<WrappedMainPage />} />
+          <Route path="/search" element={<WrappedSearchPage />} />
         </Routes>
       </BrowserRouter>
     </Suspense>
