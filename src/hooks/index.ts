c00da96afa6e3c5df64de19c09d5b11248cf4fde
dy7791 <dy7791@naver.com>
2025-03-26 16:07:06 +0900

    code split

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,5 +2,6 @@ import useAsyncError from "./fetch/useAsyncError";
 import useFetch from "./fetch/useFetch";
 import usePagination from "./common/usePagination";
 import useAddress from "./etc/useAddress";
+import useSearch from "./search/useSearch";
 
-export { useAsyncError, useFetch, usePagination, useAddress };
+export { useAsyncError, useFetch, usePagination, useAddress, useSearch };
