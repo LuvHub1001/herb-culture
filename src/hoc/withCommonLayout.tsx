f332a422e685c673c1ba79e7c9784a1bdda03d68
dy7791 <dy7791@naver.com>
2025-03-27 10:07:19 +0900

    fix:: style

--- src/hoc/withCommonLayout.tsx
+++ src/hoc/withCommonLayout.tsx
@@ -4,9 +4,9 @@ import { Header, Footer } from "../components";
 function withCommonLayout(Component: ComponentType) {
   const WrappedComponent = () => {
     return (
-      <div className="overflow-x-hidden">
+      <div className="flex flex-col min-h-screen overflow-x-hidden">
         <Header />
-        <div>
+        <div className="flex-grow">
           <Component />
         </div>
         <Footer />
