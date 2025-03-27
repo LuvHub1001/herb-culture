8dabfa07cdc15b8a5dc8bc929644fdea00bcf086
dy7791 <dy7791@naver.com>
2025-03-27 15:54:15 +0900

    test:: netlify

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -11,9 +11,15 @@ const RETRY_TIMEOUT = 1000;
 const RETRY_MAX_COUNT = 3;
 let RETRY_COUNT = 0;
 
+const baseURL =
+  import.meta.env.MODE === "production"
+    ? `/api/${import.meta.env.VITE_API_KEY}/json`
+    : "http://openapi.seoul.go.kr:8088/" +
+      import.meta.env.VITE_API_KEY +
+      "/json";
+
 const axiosInstance = axios.create({
-  baseURL:
-    "http://openapi.seoul.go.kr:8088/" + import.meta.env.VITE_API_KEY + "/json",
+  baseURL: baseURL,
   timeout: AXIOS_TIMEOUT,
   headers: {
     "Content-Type": "application/json",
