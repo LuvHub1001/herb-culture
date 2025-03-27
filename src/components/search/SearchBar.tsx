ad49b624919e3df55a47f67c505d100a2739c697
dy7791 <dy7791@naver.com>
2025-03-27 15:00:51 +0900

    fix:: SearchBar 반응형

--- src/components/search/SearchBar.tsx
+++ src/components/search/SearchBar.tsx
@@ -5,20 +5,22 @@ function SearchBar() {
     useSearch();
 
   return (
-    <div className="flex w-screen mt-10 justify-center items-center">
-      <input
-        type="text"
-        className="border-2 rounded-lg w-120 h-15 placeholder:text-lg p-2"
-        placeholder="원하시는 문화행사를 검색하세요"
-        value={searchKeyword}
-        onChange={(e) => setSearchKeyword(e.target.value)}
-        onKeyDown={(e) => handleKeyDown(e)}
-      />
-      <img
-        src="/assets/images/search.svg"
-        className="absolute right-178 cursor-pointer"
-        onClick={handleSearchButton}
-      />
+    <div className="flex w-screen mt-10 justify-center items-center max-sm:p-4">
+      <div className="relative w-120">
+        <input
+          type="text"
+          className="border-2 rounded-lg w-full h-15 placeholder:text-lg p-2 pr-12"
+          placeholder="원하시는 문화행사를 검색하세요"
+          value={searchKeyword}
+          onChange={(e) => setSearchKeyword(e.target.value)}
+          onKeyDown={(e) => handleKeyDown(e)}
+        />
+        <img
+          src="/assets/images/search.svg"
+          className="absolute right-4 top-1/2 transform -translate-y-1/2 cursor-pointer w-6 h-6"
+          onClick={handleSearchButton}
+        />
+      </div>
     </div>
   );
 }
