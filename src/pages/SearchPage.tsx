a317379a4c65c933a778d16a4fb301a138a7aeab
dy7791 <dy7791@naver.com>
2025-03-27 10:07:38 +0900

    add:: Skeleton UI

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,10 +1,23 @@
-import { SearchBar, SearchFetch } from "../components";
+import { Suspense } from "react";
+import { SearchBar, SearchFetch, EventSkeleton } from "../components";
 
 function SearchPage() {
   return (
     <>
       <SearchBar />
-      <SearchFetch />
+      <Suspense
+        fallback={
+          <div className="flex w-screen justify-center mt-10">
+            <div className="grid w-10/12 grid-cols-5 gap-6">
+              {[...Array(10)].map((_, idx) => (
+                <EventSkeleton key={idx} />
+              ))}
+            </div>
+          </div>
+        }
+      >
+        <SearchFetch />
+      </Suspense>
     </>
   );
 }
